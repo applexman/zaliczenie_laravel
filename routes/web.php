6470eaf7432312d55842ce9ce4d692f9920c2329
Marcin Koterba <marcin.koterba1@gmail.com>
2023-12-04 22:47:33 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -27,24 +27,25 @@ Route::middleware([
     })->name('dashboard');
 });
 
-Route::get('/transactions', [\App\Http\Controllers\TransactionController::class, 'index'])->name('transactions');
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::get('/transactions', [\App\Http\Controllers\TransactionController::class, 'index'])->name('transactions');
 Route::post('/transactions', [App\Http\Controllers\TransactionController::class, 'transfer'])->name('transfer');
 
-Route::get('dashboard', [App\Http\Controllers\TransactionController::class,'show'])->name('dashboard');
+Route::get('dashboard', [App\Http\Controllers\TransactionController::class, 'show'])->name('dashboard');
 
-Route::get('employee', [App\Http\Controllers\EmployeeController::class,'index'])->name('employee');
+Route::get('employee', [App\Http\Controllers\EmployeeController::class, 'index'])->name('employee');
 
-Route::get('admin.index', [App\Http\Controllers\AdminController::class,'index'])->name('admin.index');
-Route::get('admin.edit/{id}', [App\Http\Controllers\AdminController::class,'edit'])->name('admin.edit');
-Route::GET('admin.create', [App\Http\Controllers\AdminController::class,'create'])->name('admin.create');
-Route::POST('admin.create', [App\Http\Controllers\AdminController::class,'createUser'])->name('createUser');
-Route::GET('deleteUser/{id}', [App\Http\Controllers\AdminController::class,'deleteUser'])->name('deleteUser');
-Route::PUT('admin.edit/{id}', [App\Http\Controllers\AdminController::class,'editUser'])->name('editUser');
+Route::get('admin.index', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.index');
+Route::get('admin.edit/{id}', [App\Http\Controllers\AdminController::class, 'edit'])->name('admin.edit');
+Route::GET('admin.create', [App\Http\Controllers\AdminController::class, 'create'])->name('admin.create');
+Route::POST('admin.create', [App\Http\Controllers\AdminController::class, 'createUser'])->name('createUser');
+Route::GET('deleteUser/{id}', [App\Http\Controllers\AdminController::class, 'deleteUser'])->name('deleteUser');
+Route::PUT('admin.edit/{id}', [App\Http\Controllers\AdminController::class, 'editUser'])->name('editUser');
 
-Route::GET('loan.index', [App\Http\Controllers\LoanController::class,'index'])->name('loan.index');
+Route::GET('loan.index', [App\Http\Controllers\LoanController::class, 'index'])->name('loan.index');
 Route::GET('loan.get', [App\Http\Controllers\LoanController::class, 'getLoan'])->name('loan.get');
-Route::POST('loan.get', [App\Http\Controllers\LoanController::class,'addLoan'])->name('addLoan');
-Route::GET('loan.index/{id}', [App\Http\Controllers\LoanController::class,'payLoan'])->name('payLoan');
+Route::POST('loan.get', [App\Http\Controllers\LoanController::class, 'addLoan'])->name('addLoan');
+Route::GET('loan.index/{id}', [App\Http\Controllers\LoanController::class, 'payLoan'])->name('payLoan');
